9c421ba0d3f852ab9da8ffe96cfddd874e4511ca
bromowi <bromo.wibowo91@gmail.com>
2021-02-25 21:52:31 +0700

    coba deploy

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,11 +7,12 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     //
-    public function index(){
-
-        $var1 = 1;
-        $var2 = 2;
-        $result = $var1 + $var;
-        return 'ini adalah hasil penjumlahan :'.$result;
+    public function index(Request $request){
+        $comment = $request->input('comment');
+        // $var1 = 1;
+        // $var2 = 2;
+        // $result = $var1 + $var;
+        // return 'ini adalah hasil penjumlahan :'.$result;
+        return view('home', ['name' == $comment]);
     }
 }
